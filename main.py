93a52e82fd767e960aa529905040c2f01d64248a
cherrycreamsoda <hamzazain2003@gmail.com>
2025-08-26 23:40:58 +0500

    feat: add 1-minute candle data collection with price, quantity, and timestamp tracking

--- main.py
+++ main.py
@@ -2,15 +2,38 @@ import asyncio
 import websockets
 import json
 
+
 async def listen():
   url = "wss://stream.binance.com:9443/ws/btcusdt@trade"
+
   
   async with websockets.connect(url) as wss:
+    first_run = True
+    first_ever_run = True
+    prices = []
+    qualtities = []
+    timestamps = []
 
-    while True:
+    while True :
       data = await wss.recv()
-      # print(data)
       trade = json.loads(data)
-      print(trade.get('p') , " : " , trade.get('q') , " : " , trade.get('T') )
+      if first_run:
+        if first_ever_run:
+          start_time = trade.get('T')//60000
+          first_ever_run = False
+        print("waiting... " , trade.get('T'))
+        if not trade.get('T')//60000 > start_time:
+          continue
+        start_time = trade.get('T')//60000
+        first_run = False
+      if trade.get('T')//60000 > start_time:
+        break
+      prices.append(trade.get('p'))
+      qualtities.append(trade.get('q'))
+      timestamps.append(trade.get('T'))
+      print(trade.get('p') , " : " , trade.get('q') , " : " , trade.get('T')//60000 )
+    for price in prices:
+      print(price)
+      
 
 asyncio.run(listen())
