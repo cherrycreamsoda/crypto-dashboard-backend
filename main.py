84c726c431cc3309fa72b33e6a2f0aa6bc31af98
cherrycreamsoda <hamzazain2003@gmail.com>
2025-08-27 02:23:06 +0500

    refactor: simplify and clean code

--- main.py
+++ main.py
@@ -2,38 +2,31 @@ import asyncio
 import websockets
 import json
 
-
-async def listen():
-  url = "wss://stream.binance.com:9443/ws/btcusdt@trade"
-
-  
-  async with websockets.connect(url) as wss:
+async def listen(url):
+  async with websockets.connect(url) as ws:
     first_run = True
     first_ever_run = True
-    prices = []
-    qualtities = []
-    timestamps = []
+    p, q, T = [], [], []
 
     while True :
-      data = await wss.recv()
+      data = await ws.recv()
       trade = json.loads(data)
       if first_run:
         if first_ever_run:
           start_time = trade.get('T')//60000
-          first_ever_run = False
+          first_ever_run = not first_ever_run
         print("waiting... " , trade.get('T'))
         if not trade.get('T')//60000 > start_time:
           continue
         start_time = trade.get('T')//60000
-        first_run = False
+        first_run = not first_run
       if trade.get('T')//60000 > start_time:
         break
-      prices.append(trade.get('p'))
-      qualtities.append(trade.get('q'))
-      timestamps.append(trade.get('T'))
+      p.append(trade.get('p'))
+      q.append(trade.get('q'))
+      T.append(trade.get('T'))
       print(trade.get('p') , " : " , trade.get('q') , " : " , trade.get('T')//60000 )
-    for price in prices:
-      print(price)
+    return p , q , T
       
-
-asyncio.run(listen())
+asyncio.run(listen("wss://stream.binance.com:9443/ws/btcusdt@trade"))
+print("SUCESS!")
