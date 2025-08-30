76063c2de15ba94d045ec924255690c0d27d2944
cherrycreamsoda <hamzazain2003@gmail.com>
2025-08-30 15:33:12 +0500

    feat: add candle generation with mkcandle and support for multiple intervals
    - Introduced `mkcandle` function to calculate OHLCV and trade count from trades.
    - Extended `listen` function to accept a mode parameter and map it to standard intervals (1m, 5m, 15m).
    - Converted price, quantity, and timestamp values to float/int for accurate calculations.
    - Improved readability by refactoring variable handling and interval logic.

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ import asyncio
 import websockets
 import json
 
-async def listen(url):
+def mkcandle(p,q,T):
+  open = p[0]
+  low = min(p)
+  high = max(p)
+  close = p[-1]
+  volume = sum(q)
+  count_of_trades = len(p)
+
+  print(open)
+  print(low)
+  print(high)
+  print(close)
+  print(volume)
+  print(count_of_trades)
+
+async def listen(mode,url):
   async with websockets.connect(url) as ws:
+    intervals = { 1:60000, 5:300000, 15:900000}
+    interval = intervals.get(mode, 60000)
     first_run = True
     first_ever_run = True
     p, q, T = [], [], []
@@ -13,20 +30,20 @@ async def listen(url):
       trade = json.loads(data)
       if first_run:
         if first_ever_run:
-          start_time = trade.get('T')//60000
+          start_time = trade.get('T')//interval
           first_ever_run = not first_ever_run
         print("waiting... " , trade.get('T'))
-        if not trade.get('T')//60000 > start_time:
+        if not trade.get('T')//interval > start_time:
           continue
-        start_time = trade.get('T')//60000
+        start_time = trade.get('T')//interval
         first_run = not first_run
-      if trade.get('T')//60000 > start_time:
+      if trade.get('T')//interval > start_time:
         break
-      p.append(trade.get('p'))
-      q.append(trade.get('q'))
-      T.append(trade.get('T'))
-      print(trade.get('p') , " : " , trade.get('q') , " : " , trade.get('T')//60000 )
+      p.append(float(trade.get('p')))
+      q.append(float(trade.get('q')))
+      T.append(int(trade.get('T')))
+      print(trade.get('p') , " : " , trade.get('q') , " : " , trade.get('T')//interval )
     return p , q , T
       
-asyncio.run(listen("wss://stream.binance.com:9443/ws/btcusdt@trade"))
-print("SUCESS!")
+minute_data = asyncio.run(listen(1,"wss://stream.binance.com:9443/ws/btcusdt@trade"))
+minute_candle = mkcandle(*minute_data)
